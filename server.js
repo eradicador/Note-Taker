58b746ba84732b32f2e55c0d6c1036ec526ebdc3
eradicador <janisepinto@yahoo.com>
2020-07-14 20:41:40 -0400

    changed the port from 6000 to 3000 now its working

--- server.js
+++ server.js
@@ -5,16 +5,17 @@ const htmlroutes = require("./routes/htmlroutes");
 
 const app = express();
 
-const PORT = process.env.PORT || 6000;
+const PORT = process.env.PORT || 3000;
 //middleware
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static("public"));
 app.use("/api", apiroutes);
 app.use("/html", htmlroutes);
+app.use(htmlroutes);
 
 
 app.listen(PORT, () => {
     console.log(`App listening on port: ${PORT}`);
-    console.log("Ready on port 6000");
+    console.log("Ready on port 3000");
 })
